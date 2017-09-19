5aa8320d9687b017a5a35aafca453f9eb9b1b9da
Mike Wernicke <mike.wernicke@gmail.com>
2017-09-19 13:33:20 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,12 +1,12 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 var array // array for all array functions
-var addedArray, // array output foro non-destructive operations
-    element // to add to arrays
+// var addedArray, // array output foro non-destructive operations
+//    element // to add to arrays
 
 function addElementToBeginningOfArray (array, element) {
   var addedArray, // array output foro non-destructive operations
       element // to add to arrays
-//  addedArray = (element, ...array) // solution #1
+  addedArray = (element, ...array) // solution #1
 //  addedArray = array.unshift(element) // solution #2
   return [addedArray]
 }
